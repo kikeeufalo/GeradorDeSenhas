66710e05a213521dc9417ed00203fe8253edb8e7
Wellinghton O. <71161234+kikeeufalo@users.noreply.github.com>
2024-11-04 15:11:32 -0300

    Update script.js

---
+++ gerador/JS/script.js
@@ -0,0 +1,22 @@
+const tamanhoSenha = window.document.getElementById('tamanho')
+const ABC = window.document.getElementById('ABC')
+const abc = window.document.getElementById('abc')
+const nums = window.document.getElementById('123')
+const carcteresEspeciais = window.document.getElementById('@#?')
+const resultado = window.document.getElementById('resultado')
+const copiar = window.document.getElementById('copiar')
+
+let tam = 0
+
+setInterval(() => {
+    if(tamanhoSenha.value !== '') {
+        if (tamanhoSenha.value !== tam) {
+            gerarSenhas()
+        }
+        tam = tamanhoSenha.value
+    }
+},500)
+
+function gerarSenhas() {
+    console.log(tamanhoSenha.value)
+}
