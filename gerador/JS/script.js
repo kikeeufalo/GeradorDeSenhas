8a9101619f5c81c94b2aa29f0d4cfd6fa92d813d
Wellinghton O. <71161234+kikeeufalo@users.noreply.github.com>
2024-11-04 15:27:18 -0300

    Update script.js

--- gerador/JS/script.js
+++ gerador/JS/script.js
@@ -5,18 +5,32 @@ const nums = window.document.getElementById('123')
 const carcteresEspeciais = window.document.getElementById('@#?')
 const resultado = window.document.getElementById('resultado')
 const copiar = window.document.getElementById('copiar')
+const alfabeto = [
+    'A', 'B', 'C', 'D', 'E',
+    'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X', 'Y',
+    'Z'
+];
+
 
 let tam = 0 //ultimo numero visto 
 
 setInterval(() => {
     if(tamanhoSenha.value !== '') {
-        if (tamanhoSenha.value !== tam) { //faz conque nao fique se repetindo toda hora
-            gerarSenhas()
-        }
-        tam = tamanhoSenha.value
+        setTimeout(() => {
+            if (tamanhoSenha.value !== tam) { //faz conque nao fique se repetindo toda hora
+                gerarSenhas()
+            }
+            tam = tamanhoSenha.value
+        },500)
     }
 },500)
 
 function gerarSenhas() {
-    console.log(tamanhoSenha.value)
+    for (let i = 0; i < tamanhoSenha.value; i++) {
+        let senhaAleatoria = Math.floor(Math.random() * 26)
+        console.log(alfabeto[senhaAleatoria])
+    }
 }
